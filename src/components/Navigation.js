dc4c6560987e10a850be6cf60e70f5acec85bcf8
Christopher Rolfe <christopher.rolfe198@gmail.com>
2018-01-24 07:34:03 +0000

    Starting to add a more friendly navigation - this is just an initial version that needs improvement

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 const Navigation = (props) => {
   return (
     <div>
       <ul className="navigation">
         {props.items && Object.entries(props.items).map(item => {
-          return <NavigationItem key={item[0]} {...item[1]} />
+          return <NavigationItem childNav={props.childNav || false} key={item[0]} {...item[1]} />
         })}
       </ul>
     </div>
   )
 }
 
-const NavigationItem = (props) => {
-  return (
-    <li className="navigation-item" key={props.name}>
-      <a href={props.href}>
-        {props.name}
-      </a>
-      {props.children ? <Navigation items={props.children} /> : <span />}
-    </li>
-  );
+class NavigationItem extends PureComponent {
+  handleHover(e) {
+    this.itemRef.classList.add('navigation-item--active');
+  }
+
+  handleEndHover(e) {
+    this.itemRef.classList.remove('navigation-item--active');
+  }
+
+  render() {
+    return (
+      <li className="navigation-item" onMouseEnter={this.handleHover.bind(this)} onMouseLeave={this.handleEndHover.bind(this)} ref={ref => {this.itemRef = ref}} key={this.props.name}>
+        <a href={this.props.href}>
+          {this.props.name}
+        </a>
+        {this.props.children ? <Navigation childNav={true} items={this.props.children} /> : <span />}
+      </li>
+    );
+  }
 }
 
 export default Navigation;
